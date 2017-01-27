667e25fa1044ccdaf7c173b65e6565c2e2785928
Adriano Sanges <adrianosanges@hotmail.com>
2017-01-27 01:06:10 +0100

    Fix GeoPoint as String

--- src/main/scala/it/bigdataitaly/spark/twitter/TweetSentimentES.scala
+++ src/main/scala/it/bigdataitaly/spark/twitter/TweetSentimentES.scala
@@ -2,12 +2,15 @@ package it.bigdataitaly.spark.twitter
 
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.Row
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.functions.explode
 import org.apache.spark.sql.functions.udf
 import org.elasticsearch.spark.sql._
 
 import com.databricks.spark.corenlp.functions._
+
+case class Geolocation(latitude:Double,longitude:Double)
 
 object TweetSentimentES {
 
@@ -16,6 +19,12 @@ object TweetSentimentES {
   def removeUrl(commentstr:String) : String ={
     val urlPattern = "((https?|ftp|gopher|telnet|file|Unsure|http):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)".r
     urlPattern.replaceAllIn(commentstr.toLowerCase(),"").trim()     
+  }
+  
+  def getGeoPoint(geo:Row): String ={
+    val lon=geo.get(1).toString()
+    val lat=geo.get(0).toString()
+    lat+","+lon
   }
     
   def main(args: Array[String]) {
@@ -30,15 +39,16 @@ object TweetSentimentES {
     import spark.implicits._
    
     val removeUrlUDF= udf((s:String) => removeUrl(s))
+    val getGeoPointUDF= udf((geo:Row) => getGeoPoint(geo))
     
     Utils.setupLogging()
    
     val tweets=spark.read.json(fromDir+"/*")
       .select("text","user","geolocation","place","lang").where("lang == 'en'").where("geolocation != null")
-      .select(col("text"),col("user"),col("geolocation"),col("place"),explode(ssplit(removeUrlUDF(col("text")))) as "sentences",col("lang"))
-      .select(col("text"),col("user"),col("geolocation"),col("place"),col("sentences"),col("lang"),sentiment(col("sentences")) as "sentiment",tokenize(col("sentences")) as "words",pos(col("sentences")) as "pos",lemma(col("sentences")) as "lemmas",ner(col("sentences")) as "nerTags")
+      .select(col("text"),col("user"),getGeoPointUDF(col("geolocation")) as "location",col("place"),explode(ssplit(removeUrlUDF(col("text")))) as "sentences",col("lang"))
+      .select(col("text"),col("user"),col("location"),col("place"),col("sentences"),col("lang"),sentiment(col("sentences")) as "sentiment",tokenize(col("sentences")) as "words",pos(col("sentences")) as "pos",lemma(col("sentences")) as "lemmas",ner(col("sentences")) as "nerTags")
       
     
-    tweets.saveToEs("twitter-geo-sentiment/tweet")
+    tweets.saveToEs("twitter-location-xmas/tweet")
     }
   }
